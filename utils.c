9d6aa23d87be1ef6ec274a57a8efc1e24283480f
minei114 <18225071@std.stei.itb.ac.id>
2026-05-29 04:12:39 +0700

    Update utils.c

--- utils.c
+++ utils.c
@@ -3,12 +3,17 @@
 
 static unsigned int _seed = 1;
 void lcgSetSeed(unsigned int seed) { _seed = seed; }
+
 unsigned int lcgRand(unsigned int max) {
     _seed = (unsigned int)((1103515245ull * _seed + 12345ull) & 0x7fffffff);
     return _seed % max;
 }
 
-// Implementasi Binary Search untuk STI (Asumsi array sudah terurut berdasarkan URL)
+int startsWith(const char *str, const char *prefix) {
+    size_t len_pre = strlen(prefix), len_str = strlen(str);
+    return len_str < len_pre ? 0 : strncmp(prefix, str, len_pre) == 0;
+}
+
 int binarySearchURL(WebPage pages[], int size, const char* target) {
     int low = 0, high = size - 1;
     while (low <= high) {
