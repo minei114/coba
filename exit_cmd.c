65309afdcc55e76011fca002ae5329a56241242c
minei114 <18225071@std.stei.itb.ac.id>
2026-05-29 04:21:57 +0700

    Create exit_cmd.c

---
+++ exit_cmd.c
@@ -0,0 +1,14 @@
+#include "exit_cmd.h"
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_RESET "\033[0m"
+
+void cmdExit(Browser *b) {
+    printf(COLOR_RED "\n=================================\n");
+    printf("          S H A C K           \n");
+    printf("        B R O W S E R         \n");
+    printf("=================================\n" COLOR_RESET);
+    printf("Mematikan ShackBrowser...\n"); [14, 15]
+    b->running = FALSE;
+}
