0194ca0730655850aed555861b13a1ca229c024e
minei114 <18225071@std.stei.itb.ac.id>
2026-05-29 04:11:54 +0700

    Update types.h

--- types.h
+++ types.h
@@ -1,23 +1,20 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-#define MAX_URL_LEN 256
-#define MAX_CONTENT_LEN 4096
-#define MAX_WEB_PAGES 200
-#define CACHE_MAX_AMOUNT 10
-#define TABS_MAX_AMOUNT 10
-#define TAB_HISTORY_MAX 50
-#define DOWNLOAD_MAX_AMOUNT 5
-#define IDX_UNDEF -1
-
-typedef int boolean;
-#define TRUE 1
+#define MAX_URL_LEN         256
+#define MAX_CONTENT_LEN     4096
+#define MAX_WEB_PAGES       200
+#define CACHE_MAX_AMOUNT    10
+#define TRUE  1
 #define FALSE 0
 
+typedef int boolean;
+
 typedef struct {
-    int id;
-    char url[MAX_URL_LEN];
-    char content[MAX_CONTENT_LEN];
+    int     id;
+    char    url[MAX_URL_LEN];
+    char    content[MAX_CONTENT_LEN];
+    int     view_count;
     boolean active;
 } WebPage;
 
