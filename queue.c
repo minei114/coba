a1fa718ceec0fb453806df3682176b0690f9b184
minei114 <18225071@std.stei.itb.ac.id>
2026-05-29 04:46:36 +0700

    Update queue.c

--- queue.c
+++ queue.c
@@ -3,16 +3,16 @@
 #include <stdio.h>
 
 void createQueue(Queue *q) {
-    q->head = -1; q->tail = -1; q->size = 0; [11]
+    q->head = -1; q->tail = -1; q->size = 0;
 }
 
 int calculateTicks(const char *url) {
-    return (strlen(url) / 5) + 2; [10]
+    return (strlen(url) / 5) + 2;
 }
 
 int enqueue(Queue *q, const char *url) {
     if (q->size >= DOWNLOAD_MAX_AMOUNT) {
-        printf("Download tidak diterima, antrian sudah penuh.\n"); [8]
+        printf("Download tidak diterima, antrian sudah penuh.\n");
         return 0;
     }
     q->tail = (q->tail + 1) % DOWNLOAD_MAX_AMOUNT;
@@ -22,25 +22,25 @@ int enqueue(Queue *q, const char *url) {
     q->data[q->tail].ticksTotal = calculateTicks(url);
     q->data[q->tail].ticksLeft = q->data[q->tail].ticksTotal;
     q->size++;
-    printf("Download %s (%d ticks)\n", url, q->data[q->tail].ticksTotal); [12]
+    printf("Download %s (%d ticks)\n", url, q->data[q->tail].ticksTotal);
     return 1;
 }
 
 int tickQueue(Queue *q) {
     if (q->size == 0) {
-        printf("Antrian download saat ini kosong.\n"); [8]
+        printf("Antrian download saat ini kosong.\n");
         return 0;
     }
     DownloadItem *curr = &q->data[q->head];
     curr->ticksLeft--;
     
     if (curr->ticksLeft <= 0) {
-        printf("%s selesai terdownload!\n", curr->url); [8, 9]
+        printf("%s selesai terdownload!\n", curr->url);
         q->head = (q->head + 1) % DOWNLOAD_MAX_AMOUNT;
         q->size--;
         if (q->size == 0) q->head = -1, q->tail = -1;
     } else {
-        printf("Downloading %s... (%d ticks tersisa)\n", curr->url, curr->ticksLeft); [12]
+        printf("Downloading %s... (%d ticks tersisa)\n", curr->url, curr->ticksLeft);
     }
     return 1;
 }
