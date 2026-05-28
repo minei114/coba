d222b29c95bb257b133b5c56c03921b95269bb71
minei114 <18225071@std.stei.itb.ac.id>
2026-05-29 04:20:40 +0700

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,25 +1,24 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include "types.h"
+#include "adt_set.h"
+
+/* Struktur Node untuk Linked List relasi (Adjacency List) [1] */
 typedef struct Node {
     int target_id;
     struct Node* next;
 } Node;
 
+/* ADT Graph untuk menyimpan Web Graph [1] */
 typedef struct {
     Node* adjList[MAX_WEB_PAGES + 1];
 } Graph;
 
-void graphAddEdge(Graph* G, int src_id, int dest_id); // [9]
-void graphRemovePageRelations(Graph* G, int deleted_id); // [10]
+void graphInit(Graph* G);
+void graphAddEdge(Graph* G, int src_id, int dest_id);
+void graphDisplayLinkedPages(Graph* G, int src_id, WebPageSet* database);
+const char* graphGetLinkedUrl(Graph* G, int src_id, int user_choice_index, WebPageSet* database);
+void graphRemovePageRelations(Graph* G, int deleted_id);
+boolean graphIsNumericValid(const char* str);
 
---------------------------------------------------------------------------------
-5. Fitur F06, F07, F08: adt_array.h/c dan navigation.h/c
-Mengelola tab (ADT Array) dan navigasi back/forward (ADT Stack)
-.
-navigation.c
-void pushNewPage(Navigation *N, const char *url) {
-    if (N->currentIndex < N->idxTop) N->idxTop = N->currentIndex; // Potong forward history [13]
-    if (N->idxTop < TAB_HISTORY_MAX - 1) {
-        N->idxTop++;
-        N->currentIndex = N->idxTop;
-        strcpy(N->buffer[N->idxTop], url);
-    }
-}
+#endif
