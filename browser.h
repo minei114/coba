c007d9d57b16dc7349602df90340c7c637bc461c
minei114 <18225071@std.stei.itb.ac.id>
2026-05-29 04:54:33 +0700

    Update browser.h

--- browser.h
+++ browser.h
@@ -1,14 +1,25 @@
 #ifndef BROWSER_H
 #define BROWSER_H
+
+#include "types.h"
 #include "adt_set.h"
 #include "adt_map.h"
+#include "graph.h"
+#include "queue.h"
+#include "adt_array.h"
 
 typedef struct {
     WebPageSet pages;
-    CacheMap   cache;
-    boolean    running;
+    CacheMap cache;
+
+    Graph graph;
+    Queue downloadQueue;
+    TabArray tabs;
+
+    boolean running;
 } Browser;
 
 void openPage(Browser *b, const char *url);
 void cmdDiscover(Browser *b);
+
 #endif
