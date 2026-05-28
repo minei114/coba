621b626c04b64ed3433099a4fbd3e6d2c4b84bc6
minei114 <18225071@std.stei.itb.ac.id>
2026-05-29 04:13:46 +0700

    Update adt_map.h

--- adt_map.h
+++ adt_map.h
@@ -1,11 +1,21 @@
+#ifndef ADT_MAP_H
+#define ADT_MAP_H
+#include "types.h"
+
 typedef struct {
-    char key[MAX_URL_LEN];
-    char value[MAX_CONTENT_LEN];
-    int order; // LRU Order
+    char    key[MAX_URL_LEN];
+    char    value[MAX_CONTENT_LEN];
+    int     order; 
     boolean used;
 } MapEntry;
 
 typedef struct {
     MapEntry entries[CACHE_MAX_AMOUNT];
-    int counter; // Untuk increment order
+    int      size;
+    int      counter;
 } CacheMap;
+
+void mapInit(CacheMap *m);
+void mapPut(CacheMap *m, const char *key, const char *value);
+char *mapGet(CacheMap *m, const char *key);
+#endif
