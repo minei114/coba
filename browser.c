5647e892de1ee67ac8783b61e305860503f6c418
minei114 <18225071@std.stei.itb.ac.id>
2026-05-29 05:11:05 +0700

    Update browser.c

--- browser.c
+++ browser.c
@@ -16,17 +16,3 @@ void openPage(Browser *b, const char *url) {
         printf("404 Not Found! Halaman tidak ditemukan.\n");
     }
 }
-
-void cmdDiscover(Browser *b) {
-    int activeIdx[MAX_WEB_PAGES], total = 0;
-    for (int i = 0; i < MAX_WEB_PAGES; i++) {
-        if (b->pages.pages[i].active) activeIdx[total++] = i;
-    }
-    if (total == 0) return;
-    int show = (total < 5) ? total : 5;
-    printf("Berikut adalah beberapa halaman yang mungkin menarik untukmu:\n");
-    for (int i = 0; i < show; i++) {
-        int r = lcgRand(total); // [1, 12]
-        printf("- %s\n", b->pages.pages[activeIdx[r]].url);
-    }
-}
