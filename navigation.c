88c2246192f0b856deea5ddda82fb029362221de
minei114 <18225071@std.stei.itb.ac.id>
2026-05-29 04:17:01 +0700

    Update navigation.c

--- navigation.c
+++ navigation.c
@@ -12,27 +12,3 @@ void pushNewPage(Navigation *N, const char *url) {
         strcpy(N->buffer[N->idxTop], url);
     }
 }
-history_view.c
-#include "history_view.h"
-#include <stdio.h>
-
-void displayTabHistory(Navigation N) {
-    if (N.idxTop == IDX_UNDEF) {
-        printf("Riwayat kosong.\n");
-        return;
-    }
-    for (int i = 0; i <= N.idxTop; i++) {
-        printf("[%d] %s", i, N.buffer[i]);
-        if (i == N.currentIndex) printf(" <- YOU ARE HERE"); // Penanda posisi [13, 14]
-        printf("\n");
-    }
-}
-
-void jumpBack(Navigation *N, int steps, char *outUrl) {
-    if (N->currentIndex - steps < 0) {
-        printf("ERROR: BACK TIDAK BISA DIJALANKAN!\n");
-        return;
-    }
-    N->currentIndex -= steps;
-    strcpy(outUrl, N->buffer[N->currentIndex]);
-}
