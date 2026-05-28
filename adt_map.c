0fbb9df782fa4e681a31e9896579a734f6e8fa6f
minei114 <18225071@std.stei.itb.ac.id>
2026-05-29 05:20:55 +0700

    Update adt_map.c

--- adt_map.c
+++ adt_map.c
@@ -4,44 +4,73 @@
 void mapInit(CacheMap *m) {
     m->size = 0;
     m->counter = 0;
-    for (int i = 0; i < CACHE_MAX_AMOUNT; i++) m->entries[i].used = FALSE;
+
+    for (int i = 0; i < CACHE_MAX_AMOUNT; i++) {
+        m->entries[i].used = FALSE;
+    }
 }
 
 char *mapGet(CacheMap *m, const char *key) {
     for (int i = 0; i < CACHE_MAX_AMOUNT; i++) {
-        if (m->entries[i].used && strcmp(m->entries[i].key, key) == 0) {
-            m->entries[i].order = ++m->counter; // Update LRU order [9]
+
+        if (m->entries[i].used &&
+            strcmp(m->entries[i].key, key) == 0) {
+
+            m->entries[i].order = ++m->counter;
             return m->entries[i].value;
         }
     }
+
     return NULL;
 }
 
 void mapPut(CacheMap *m, const char *key, const char *value) {
-    int idx = -1, minOrder = m->counter + 1;
-    if (m->size >= CACHE_MAX_AMOUNT) { // LRU Victim Search [6]
+
+    int idx = -1;
+    int minOrder = m->counter + 1;
+
+    if (m->size >= CACHE_MAX_AMOUNT) {
+
         for (int i = 0; i < CACHE_MAX_AMOUNT; i++) {
+
             if (m->entries[i].order < minOrder) {
                 minOrder = m->entries[i].order;
                 idx = i;
             }
         }
+
     } else {
+
         for (int i = 0; i < CACHE_MAX_AMOUNT; i++) {
-            if (!m->entries[i].used) { idx = i; break; }
+
+            if (!m->entries[i].used) {
+                idx = i;
+                break;
+            }
         }
+
         m->size++;
     }
+
     m->entries[idx].used = TRUE;
     m->entries[idx].order = ++m->counter;
+
     strncpy(m->entries[idx].key, key, MAX_URL_LEN);
     strncpy(m->entries[idx].value, value, MAX_CONTENT_LEN);
 }
-void mapRemove(CacheMap *m, char *url) {
+
+void mapRemove(CacheMap *m, const char *url) {
+
     for (int i = 0; i < CACHE_MAX_AMOUNT; i++) {
-        if (strcmp(m->entries[i].url, url) == 0) {
-            m->entries[i].url[0] = '\0';
-            m->entries[i].content[0] = '\0';
+
+        if (m->entries[i].used &&
+            strcmp(m->entries[i].key, url) == 0) {
+
+            m->entries[i].used = FALSE;
+            m->entries[i].key[0] = '\0';
+            m->entries[i].value[0] = '\0';
+
+            m->size--;
         }
     }
 }
