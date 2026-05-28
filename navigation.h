1211b8b7f36f2925fb9b1ee0a68df82cc76d73f2
minei114 <18225071@std.stei.itb.ac.id>
2026-05-29 05:24:30 +0700

    Update navigation.h

--- navigation.h
+++ navigation.h
@@ -1,19 +1,24 @@
 #ifndef NAVIGATION_H
 #define NAVIGATION_H
+
 #include "types.h"
 
 /* ADT Stack untuk navigasi per tab */
 typedef struct {
     char buffer[TAB_HISTORY_MAX][MAX_URL_LEN];
-    int idxTop;       /* Elemen tertinggi di stack */
-    int currentIndex; /* Posisi user sekarang */
+    int idxTop;       /* index tertinggi di stack */
+    int currentIndex; /* posisi halaman saat ini */
 } Navigation;
 
 void CreateNav(Navigation *N);
-/* Push halaman baru. Hapus history ke depan jika kursor di tengah */
+
+/* Push halaman baru. Hapus forward history jika berada di tengah */
 void pushNewPage(Navigation *N, const char *url);
-/* Mundur/Maju 1 langkah */
+
+/* Mundur 1 langkah */
 void stepBack(Navigation *N, char *outUrl);
+
+/* Maju 1 langkah */
 void stepForward(Navigation *N, char *outUrl);
 
 #endif
