d1689e76473ecaa0e08d72d280d1036a2eaaf4ca
minei114 <18225071@std.stei.itb.ac.id>
2026-05-29 05:20:39 +0700

    Update adt_set.c

--- adt_set.c
+++ adt_set.c
@@ -2,38 +2,66 @@
 #include <string.h>
 
 void setInit(WebPageSet *s) {
+
     s->size = 0;
     s->nextId = 1;
-    for (int i = 0; i < MAX_WEB_PAGES; i++) s->pages[i].active = FALSE;
+
+    for (int i = 0; i < MAX_WEB_PAGES; i++) {
+        s->pages[i].active = FALSE;
+    }
 }
 
 WebPage *setSearch(WebPageSet *s, const char *url) {
+
     for (int i = 0; i < MAX_WEB_PAGES; i++) {
-        if (s->pages[i].active && strcmp(s->pages[i].url, url) == 0) {
+
+        if (s->pages[i].active &&
+            strcmp(s->pages[i].url, url) == 0) {
+
             return &s->pages[i];
         }
     }
+
     return NULL;
 }
-boolean setInsert(WebPageSet *s, char *url, char *content) {
+
+boolean setInsert(WebPageSet *s,
+                  const char *url,
+                  const char *content) {
+
     for (int i = 0; i < MAX_WEB_PAGES; i++) {
+
         if (!s->pages[i].active) {
-            s->pages[i].id = i;
+
+            s->pages[i].id = s->nextId++;
+
             strcpy(s->pages[i].url, url);
             strcpy(s->pages[i].content, content);
+
             s->pages[i].view_count = 0;
             s->pages[i].active = TRUE;
+
+            s->size++;
+
             return TRUE;
         }
     }
+
     return FALSE;
 }
-void setDelete(WebPageSet *s, char *url) {
+
+void setDelete(WebPageSet *s, const char *url) {
+
     for (int i = 0; i < MAX_WEB_PAGES; i++) {
+
         if (s->pages[i].active &&
             strcmp(s->pages[i].url, url) == 0) {
 
             s->pages[i].active = FALSE;
+
+            s->size--;
+
+            return;
         }
     }
 }
